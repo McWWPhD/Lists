2b206f04e99d1d06612812ab20b038d135e45bc6
wojciech w <mcwwphd@gmail.com>
2020-05-13 21:11:54 +0200

    Add project files.

---
+++ Lists/Form1.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace Lists
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBoxL = new System.Windows.Forms.ListBox();
+            this.listBoxR = new System.Windows.Forms.ListBox();
+            this.btnToR = new System.Windows.Forms.Button();
+            this.btnToL = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxL
+            // 
+            this.listBoxL.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listBoxL.FormattingEnabled = true;
+            this.listBoxL.ItemHeight = 16;
+            this.listBoxL.Location = new System.Drawing.Point(54, 28);
+            this.listBoxL.Name = "listBoxL";
+            this.listBoxL.Size = new System.Drawing.Size(258, 388);
+            this.listBoxL.TabIndex = 0;
+            // 
+            // listBoxR
+            // 
+            this.listBoxR.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.listBoxR.FormattingEnabled = true;
+            this.listBoxR.ItemHeight = 16;
+            this.listBoxR.Location = new System.Drawing.Point(483, 28);
+            this.listBoxR.Name = "listBoxR";
+            this.listBoxR.Size = new System.Drawing.Size(258, 388);
+            this.listBoxR.TabIndex = 1;
+            // 
+            // btnToR
+            // 
+            this.btnToR.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnToR.Location = new System.Drawing.Point(349, 128);
+            this.btnToR.Name = "btnToR";
+            this.btnToR.Size = new System.Drawing.Size(98, 63);
+            this.btnToR.TabIndex = 2;
+            this.btnToR.Text = ">>";
+            this.btnToR.UseVisualStyleBackColor = true;
+            this.btnToR.Click += new System.EventHandler(this.ToTheRight);
+            // 
+            // btnToL
+            // 
+            this.btnToL.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.btnToL.Location = new System.Drawing.Point(349, 273);
+            this.btnToL.Name = "btnToL";
+            this.btnToL.Size = new System.Drawing.Size(98, 63);
+            this.btnToL.TabIndex = 3;
+            this.btnToL.Text = "<<";
+            this.btnToL.UseVisualStyleBackColor = true;
+            this.btnToL.Click += new System.EventHandler(this.ToTheLeft);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 441);
+            this.Controls.Add(this.btnToL);
+            this.Controls.Add(this.btnToR);
+            this.Controls.Add(this.listBoxR);
+            this.Controls.Add(this.listBoxL);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxL;
+        private System.Windows.Forms.ListBox listBoxR;
+        private System.Windows.Forms.Button btnToR;
+        private System.Windows.Forms.Button btnToL;
+    }
+}
+
---
+++ Lists/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lists
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 1; i < 20; i++)
+            {
+                listBoxL.Items.Add(string.Format("Opcja {0}", i));
+                listBoxR.Items.Add(string.Format("Opcja {0}", (char)(i + (int)'A')) );
+            }
+
+            listBoxL.SelectionMode = SelectionMode.MultiExtended;
+            listBoxR.SelectionMode = SelectionMode.MultiExtended;
+        }
+
+        private void ToTheRight(object sender, EventArgs e)
+        {
+            do
+            {
+                listBoxR.Items.Add(listBoxL.SelectedItem);
+                listBoxL.Items.Remove(listBoxL.SelectedItem);
+
+            } while (listBoxL.SelectedIndices.Count > 0);
+
+        }
+
+        private void ToTheLeft(object sender, EventArgs e)
+        {
+            do
+            {
+                listBoxL.Items.Add(listBoxR.SelectedItem);
+                listBoxR.Items.Remove(listBoxR.SelectedItem);
+
+            } while (listBoxR.SelectedIndices.Count > 0);
+
+
+        }
+    }
+
+
+
+
+}
